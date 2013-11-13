92998c74c6bbe4817498acf659db4bc4c2695a43
Boris Egorov <jightuse@gmail.com>
2013-11-13 13:05:08 +0700

    Add utility to create REALLY large files
    Utility largefile seeks to the end of the file at the large offset and
    writes one byte to this position. It creates huge file (~16TB).

---
+++ src/largefile.c
@@ -0,0 +1,55 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "files.h"
+
+int create_large_file(char *fname, off64_t sz);
+
+int main(int argc, char *argv[])
+{
+    char *fname;
+    off64_t kilobyte = 1024;
+    off64_t megabyte = kilobyte * kilobyte;
+    off64_t terabyte = megabyte * megabyte;
+    off64_t offset = 15 * terabyte;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <new_file_name>\n", argv[0]);
+        return 1;
+    }
+    fname = argv[1];
+
+    if (!create_large_file(fname, offset)) {
+        off_t fsz = get_filesize(fname);
+        printf("Success!\n");
+        if (fsz > 0) {
+            printf("Created file '%s' with size %ld bytes\n", fname, fsz);
+        }
+    }
+    return 0;
+}
+
+int create_large_file(char *fname, off64_t sz)
+{
+    int fd = open(fname, O_WRONLY | O_CREAT | O_LARGEFILE, 0644);
+
+    if (fd < 1) {
+        perror("open");
+        return 1;
+    }
+
+    if (lseek64(fd, sz, SEEK_END) < 1) {
+        perror("lseek");
+        return 1;
+    }
+
+    if (write(fd, "0", 1) < 1) {
+        perror("write");
+        return 1;
+    }
+    close(fd);
+    return 0;
+}
