2662379a02239ac8da9662cf85ca4dbff342bf8e
Boris Egorov <egorov@linux.com>
2015-07-07 22:26:27 +0600

    tmpfile: Use label to avoid repeating

--- src/tmpfile.c
+++ src/tmpfile.c
@@ -7,28 +7,31 @@
 
 int main(int argc, char *argv[])
 {
-    int fd;
+    int fd = -1;
+    int retval = 1;
     const char *buf = "Hello temporary file!";
     size_t buffer_length = strlen(buf);
 
     if (argc != 1) {
         fprintf(stderr, "Usage: %s\n", argv[0]);
-        return 1;
+        goto out;
     }
 
     fd = open(".", O_TMPFILE | O_RDWR, S_IRUSR | S_IWUSR);
     if (fd == -1) {
         perror("open");
-        return 1;
+        goto out;
     }
     printf("Temporary file fd: %d\n", fd);
 
     if (buffer_length != writeall(fd, buf, buffer_length)) {
         perror("writeall");
-        close(fd);
-        return 1;
+        goto out;
     }
 
-    close(fd);
-    return 0;
+    retval = 0;
+out:
+    if (fd > 0)
+        close(fd);
+    return retval;
 }
